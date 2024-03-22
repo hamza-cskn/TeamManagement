81c693e91044e517b12de82c47de0a0790fa7c8d
hamza <hamzacoskun41@gmail.com>
2024-03-22 10:03:27 +0300

    feat: add Department and Role properties for user.

--- backend/User/User.cs
+++ backend/User/User.cs
@@ -4,10 +4,11 @@ namespace backend.User;
 
 [BsonIgnoreExtraElements]
 public class User : Identifiable
-
 {
     public UserName Name { get; set; }
     public string Mail { get; set; }
+    public string? Department { get; set; }
+    public string? Role { get; set; }
     public string? Image { get; set; }
     public List<UserPermission> Permissions { get; set; }
     public string? Password { get; set; }
