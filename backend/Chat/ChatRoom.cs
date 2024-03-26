fcb114a83b7c9b93eb13cc831e40b46c67186c98
hamza <hamzacoskun41@gmail.com>
2024-03-26 12:29:30 +0300

    feat: add chat type.

--- backend/Chat/ChatRoom.cs
+++ backend/Chat/ChatRoom.cs
@@ -4,7 +4,21 @@ public class ChatRoom : Identifiable
 {
     
     public string Name { get; set; }
-    public List<Guid> Members { get; set; }
+    public List<Guid> Participants { get; set; }
+    public ChatType Type { get; set; }
     
     public ChatRoom() {}
+    
+    public ChatRoom(string name, List<Guid> participants)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Participants = participants;
+    }
+}
+
+public enum ChatType
+{
+    Group,
+    Direct
 }
